e1e22e99a6580673065ef5e0a428430262d15d8e
PeaceChanpornpakdee <yothinpeace@gmail.com>
2021-02-18 15:15:22 +0700

    Button Matrix Function Finished

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -44,6 +44,9 @@ UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
 
+uint16_t ButtonMatrixState = 0;
+uint32_t ButtonMatrixTimeStamp = 0;
+
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -51,6 +54,8 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
 /* USER CODE BEGIN PFP */
+
+void ButtonMatrixUpdate(); //Scan and Update data of Button Matrix
 
 /* USER CODE END PFP */
 
@@ -102,7 +107,7 @@ int main(void)
   //--------------------------------------------------------------------------------
   while (1)
   {
-
+	  ButtonMatrixUpdate();
 
 
 
@@ -270,6 +275,37 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+//Function Implementation
+GPIO_TypeDef* ButtonMatrixPort[8] = {GPIOA,       GPIOB,      GPIOB,      GPIOB,             GPIOA,      GPIOC,      GPIOB,      GPIOA     };
+uint16_t      ButtonMatrixPin [8] = {GPIO_PIN_10, GPIO_PIN_3, GPIO_PIN_5, GPIO_PIN_4,        GPIO_PIN_9, GPIO_PIN_7, GPIO_PIN_6, GPIO_PIN_7};
+uint8_t       ButtonMatrixLine    = 0;
+void ButtonMatrixUpdate()
+{
+	if(HAL_GetTick() - ButtonMatrixTimeStamp >= 100)
+	{
+		ButtonMatrixTimeStamp = HAL_GetTick();
+		for(int i = 0; i < 4; i++)
+		{
+			GPIO_PinState PinState = HAL_GPIO_ReadPin(ButtonMatrixPort[i], ButtonMatrixPin[i]);
+			if(PinState == GPIO_PIN_RESET) // Button is Pressed
+			{
+				ButtonMatrixState |= (uint16_t)1 << (i + ButtonMatrixLine * 4);
+
+			}
+			else
+			{
+				ButtonMatrixState &= ~((uint16_t)1 << (i + ButtonMatrixLine * 4));
+
+			}
+		}
+		uint8_t NowOutputPin = ButtonMatrixLine + 4;
+		HAL_GPIO_WritePin(ButtonMatrixPort[NowOutputPin], ButtonMatrixPin[NowOutputPin], GPIO_PIN_SET);
+		ButtonMatrixLine = (ButtonMatrixLine + 1) % 4;
+
+		uint8_t NextOutputPin = ButtonMatrixLine + 4;
+		HAL_GPIO_WritePin(ButtonMatrixPort[NextOutputPin], ButtonMatrixPin[NextOutputPin], GPIO_PIN_RESET);
+	}
+}
 
 /* USER CODE END 4 */
 
